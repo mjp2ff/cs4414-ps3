fb7fe5f172c9d78cc59ee1f9a64e0ca925727b2e
Matt PB <mjp2ff@virginia.edu>
2014-02-27 00:25:12 -0500

    Changed test back to 60 requests. Expect roughly ~12.5 seconds on benchmark.

--- benchmark.sh
+++ benchmark.sh
@@ -2,16 +2,16 @@
 # Run benchmarking 10 times
 ./zhtta &
 T=$(date +%s%N);
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
-httperf --server localhost --port 4414 --rate 120 --num-conns 120 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
+httperf --server localhost --port 4414 --rate 60 --num-conns 60 --wlog=y,./zhtta-test-NUL.txt | grep -e "Total: connections" -e "1xx"
 T=$(($(date +%s%N)-T));
 pkill zhtta
 printf "\n10 tests completed in %d.%d seconds.\n" "$((T/1000000000))" "$((T/1000000%1000))"
